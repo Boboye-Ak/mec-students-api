089df72bc8b5746103ffb30c3942ff5d2512e2a3
Boboye-Ak <boboye771@gmail.com>
2021-10-20 22:43:51 +0100

    second deploy

--- models/student.js
+++ models/student.js
@@ -5,7 +5,7 @@ const studentSchema=mongoose.Schema({
     firstname:{type: String, required:true},
     aka:{type: String, required:true},
     dob:{type:Date, required:true},
-    imgsrc:{type:String, required:true},
+    imgsrc:{type:String, required:false},
     sociallinks:{type:String, required:true},
     email:{type:String, required:true}
 
